d9b2b8b993090f6bfeac5382a2717d91a80fec27
jhsilvaz13 <jhsilvaz@unal.edu.co>
2024-03-18 13:43:18 -0500

    Finished bord matrix

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-from src.video.video import start
+from src.video.video import threshold_image, resize_image, to_matrix
+from src.video.boardcapture import WindowCapture as b_Cap
 if __name__ == "__main__":
-    start()
+    boardcap = b_Cap()
+    while(True):
+        # get an updated image of the game
+        """
+        hold = holdcap.get_screenshot()
+        hold = threshold_image(hold)
+        hold = resize_image(hold, 10, 22)
+        hold = to_matrix(hold)
+        hold.print_board()
+        """
+        board = boardcap.get_screenshot()
+        board = threshold_image(board)
+        board = resize_image(board, 10, 22)
+        board = to_matrix(board)
+        board.print_board()
+        """
+        next = nextcap.get_screenshot()
+        next = threshold_image(next)
+        next = resize_image(next, 10, 22)
+        next = to_matrix(next)
+        next.print_board()
+        """
--- src/video/video.py
+++ src/video/video.py
@@ -1,10 +1,6 @@
 import cv2 as cv
 import numpy as np
-from src.video.boardcapture import WindowCapture as h_Cap
-from src.video.boardcapture import WindowCapture as b_Cap
-from src.video.boardcapture import WindowCapture as n_Cap
 from src.matrix.matrix import Matrix
-import time
 
 def resize_image(image:np.ndarray, width:int, height:int) -> np.ndarray:
     """
@@ -21,34 +17,5 @@ def threshold_image(image: np.ndarray) -> np.ndarray:
     _, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
     return thresh
 
-def to_matrix(image) -> Matrix:
+def to_matrix(image:np.ndarray) -> Matrix:
     return Matrix(image)
-
-    
-
-def start():
-    holdcap = h_Cap()
-    boardcap = b_Cap()
-    nextcap = n_Cap()
-    while(True):
-        # get an updated image of the game
-        """
-        hold = holdcap.get_screenshot()
-        hold = threshold_image(hold)
-        hold = resize_image(hold, 10, 22)
-        hold = to_matrix(hold)
-        hold.print_board()
-        """
-        board = boardcap.get_screenshot()
-        board = threshold_image(board)
-        board = resize_image(board, 10, 22)
-        board = to_matrix(board)
-        board.print_board()
-        """
-        next = nextcap.get_screenshot()
-        next = threshold_image(next)
-        next = resize_image(next, 10, 22)
-        next = to_matrix(next)
-        next.print_board()
-        """
-print('Done.')
